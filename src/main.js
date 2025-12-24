a7275b9b00118f90399de8b72f8e7a102458b987
MichaelPopov62 <romantikzizni@gmail.com>
2025-12-24 16:38:03 +0200

    changed main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import './css/modal.js';
-import './css/menu.js';
+import './js/modal.js';
+import './js/menu.js';
+
 import './css/styles.css';
 import './css/header.css';
+// CSS
+import './css/reset.css';
+import './css/base.css';
+import './css/container.css';
+import './css/header.css';
 import './css/footer.css';
+import './css/animations.css';
+import './css/badges.css';
+import './css/back-link.css';
+import './css/vite-promo.css';
+
+
